d1b4af6aa4889abc2d93348997e328bf7e1a779c
Adriano Costa <adriano.dmcosta@gmail.com>
2024-12-02 20:42:53 -0300

    reduzir infra minima cluster

--- eks-nodeg.tf
+++ eks-nodeg.tf
@@ -3,13 +3,13 @@ resource "aws_eks_node_group" "node-group" {
   node_group_name = "NG-${var.projectName}"
   node_role_arn   = data.aws_iam_role.labrole.arn
   subnet_ids      = [for subnet in data.aws_subnet.subnet : subnet.id if subnet.availability_zone != "${var.regionDefault}e"]
-  disk_size       = 50
+  disk_size       = 20
   instance_types  = [var.instanceType]
 
   scaling_config {
     desired_size = 2
-    max_size     = 5
-    min_size     = 2
+    max_size     = 3
+    min_size     = 1
   }
 
   update_config {
